c32c305b51eaaf34d6be08fdab1d87851757d390
Illya King <illyaking1978@gmail.com>
2020-05-02 13:57:41 -0700

    Redo of the project

---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top" style="background-color: #000000!important">
+        <a class="navbar-brand" href="#">ACE IN THE HOLE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-link">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-link">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-link">
+                    <a class="nav-link" href="#">FAQs</a>
+                </li>
+                <li class="nav-link">
+                    <a class="nav-link" href="#">Registration</a>
+                </li>
+                <li class="nav-link">
+                    <a class="nav-link" href="#">Races</a>
+                </li>
+                <li class="nav-link">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
